dca842ce0ba39b27ec3579cdcf18a6478ce470b7
BigBirdTheGreat <76016852+BigBirdTheGreat@users.noreply.github.com>
2022-02-02 12:03:36 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,39 @@
 const menu = document.getElementById("menu");
+
 const exit = document.getElementById("exit");
-const contactLink = document.getElementById("contact")
-const navLinks = document.querySelector(".navlinks")
+
+const contactLink = document.getElementById("contact");
+
+const navLinks = document.querySelector(".navlinks");
+
+const scrollelem = document.querySelectorAll(".js-scroll");
+const scrollOffset = 100;
+
+const elementInView = (el, offset = 0) => {
+    const elementTop = el.getBoundingClientRect().top;
+    return(
+        elementTop <= ((window.innerHeight || document.documentElement.clientHeight) - scrollOffset)
+    );
+};
+const displayScrollElement = (element) => {
+    element.classList.add("scrolled");
+};
+
+const handleScrollAnimation = () => {
+    scrollelem.forEach((el) => {
+
+        if (elementInView(el, scrollOffset)){
+        
+            displayScrollElement(el);
+        }
+
+    })
+}
+
+
+window.addEventListener('scroll', () => {
+    handleScrollAnimation();
+  })
 
 function closenavbar(){
 
@@ -15,10 +47,10 @@ menu.addEventListener("click", () => {
     navLinks.style.display = "block";
     menu.style.display = "none";
     exit.style.display = "block";
-})
+});
 
 exit.addEventListener("click", () => {
     closenavbar();
-})
+});
 
 
